e1179872c86d1a0847a0e21d7092338de76b5711
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-02-27 13:10:25 +0100

    DPC Bullettin parsing adpatation

--- parseDescription.py
+++ parseDescription.py
@@ -1,9 +1,8 @@
 def parseAccertati(desc):
     accertati = desc.split('. ')[0]
     casi = []
-    for caso in accertati.replace('ed',',').split(', '):
-        casi.append(caso.replace('i casi accertati di Coronavirus in ','').replace(' sono',':').replace(' in ',': ').replace(' nel ',': ').replace(' nella ',': ').replace(' nelle ',': ').replace(' e ',', ').replace(' a ',': ').replace('uno','1').replace('una','1').strip() )
-    
+    for caso in accertati.replace(' ed',',').replace(' e',',').split(', '):
+        casi.append(caso.replace('i casi accertati di Coronavirus in ','').replace(' sono',':').replace(' in ',': ').replace(' nel ',': ').replace(' nella ',': ').replace(' nelle ',': ').replace(' a ',': ').replace('uno','1').replace('una','1').strip() )
     data = []
     for i,val in enumerate(casi):
         if i > 0:
