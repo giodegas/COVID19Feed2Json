be62622c4c83f85208f95873cf3bf69880795b16
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-02-28 09:38:10 +0100

    Manual geocoding

--- parseDescription.py
+++ parseDescription.py
@@ -1,5 +1,4 @@
 def parseAccertati(desc):
-    import geocoder
     accertati = desc.split('. ')[0]
     casi = []
     for caso in accertati.replace(' ed',',').replace(' e',',').split(', '):
@@ -14,14 +13,45 @@ def parseAccertati(desc):
             # print(val.split(': '))
             location_name = val.split(': ')[::-1][1]
             case_count = int(val.split(': ')[::-1][0])
-        # Geocode
-        g = geocoder.arcgis(location_name+", Italy")
-        latlng = g.json['raw']['feature']['geometry']
-        dict = { location_name : case_count, 'lat': latlng['y'], 'lng': latlng['x'] }
+            # Geocode
+        coords = geocodeRegione(location_name)
+        dict = { "type":"Feature", "properties": {"regione": location_name, "casi": case_count}, "geometry": {"type":"Point", "coordinates":  coords }}
         data.append(dict)
-    #print(data)   
+    print(data)   
     return data
 
+def geocodeRegione(regione):
+    coordinates = ""
+    if regione == 'Lombaria':
+        coordinates = [9.227828979492188,45.57463894211682]
+    elif regione == 'Veneto':
+        coordinates = [11.854319842000052,45.65492501300008]
+    elif regione == 'Emilia-Romagna':
+        coordinates = [11.039213647000054,44.52591084900007]
+    elif regione == 'Liguria':
+        coordinates = [9.427370000000053,44.415350258000046]
+    elif regione == 'Sicilia':
+        coordinates = [14.000000000000057,37.50000000000006]
+    elif regione == 'Marche':
+        coordinates = [13.137824067000054,43.34820017700008]
+    elif regione == 'Lazio':
+        coordinates = [12.772626321000075,41.97568188900004]
+    elif regione == 'Campania':
+        coordinates = [14.840115810000043,40.85973483500004]
+    elif regione == 'Piemonte':
+        coordinates = [7.92049247500006,45.05730108000006]
+    elif regione == 'Toscana':
+        coordinates = [11.126187689000062,43.45082948600003]
+    elif regione == 'Abruzzo':
+        coordinates = [13.855048784000076,42.22755568500003]
+    elif regione == 'Puglia':
+        coordinates = [16.618785484000057,40.98504106300004]
+    elif regione.find('Bolzano'):
+        coordinates = [11.121170080500887,46.072160043857124]
+
+    return coordinates
+
+    
 def parseRicoverati(desc):
     import re
     pazienti = desc.split('. ')[1]
